eaded1886e71b7de27bf159ecc3a407bf53f522c
Sapoleone <154547272+Sapoleone@users.noreply.github.com>
2025-03-26 22:06:07 +0100

    Implement data persistence for categories and payment options, improve data loading, and add transaction removal

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -2,11 +2,13 @@ import {Transaction} from "../models/Transaction.js";
 import {Conto} from "../models/Conto.js";
 import * as UI from "../view/ui.js";
 import * as Memory from "../models/jsonManager.js";
+import {Category} from "../models/Category.js";
+import {PaymentOptions} from "../models/PaymentOptions.js";
 
-const nullAccount = new Conto("__none__", -1, "(-)");
-let currentAccount;
+const nullAccount = new Conto("__none__", -1, "(-)"); // Conto di default per quando non ci sono conti
+let currentAccount; // Conto corrente selezionato
 
-loadData()
+initMemory() // Inizializza la memoria
 
 //TODO: rem test
 // noinspection PointlessBooleanExpressionJS
@@ -17,39 +19,113 @@ if(false){
     UI.updateAccountList(Conto.accountList, currentAccount);
 }
 
+/**
+ * Crea una nuova transazione e la aggiunge al conto corrente.
+ * @param {number} value - Il valore della transazione.
+ * @param {string} type - Il tipo della transazione.
+ * @param {string} date - La data della transazione.
+ */
 export function createTransaction(value, type, date) {
-    let transaction = new Transaction(value, type, date);
-    currentAccount.addTransaction(transaction);
-    UI.showConto(currentAccount);
-}
-export function addConto(name, value, currency){
-    currentAccount = new Conto(name, value, currency);
-    UI.updateAccountList(Conto.accountList, currentAccount);
-}
-export function remConto(id){
-    Conto.accountList.forEach((account, i) => {
-        if(account.id === id) Conto.accountList.splice(i, 1);
-    });
-    currentAccount = (Conto.accountList.length > 0)? Conto.accountList[0]: nullAccount;
-    UI.updateAccountList(Conto.accountList, currentAccount);
+    let transaction = new Transaction(value, type, date); // Crea una nuova transazione
+    currentAccount.addTransaction(transaction); // Aggiunge la transazione al conto corrente
+    UI.showConto(currentAccount); // Aggiorna la UI
 }
 
-// export function getAccounts(){ return Conto.accountList; }
+/**
+ * Aggiunge un nuovo conto alla lista dei conti.
+ * @param {string} name - Il nome del conto.
+ * @param {number} value - Il saldo iniziale del conto.
+ * @param {string} currency - La valuta del conto.
+ */
+export function addConto(name, value, currency){
+    currentAccount = new Conto(name, value, currency); // Crea un nuovo conto
+    UI.updateAccountList(Conto.accountList, currentAccount); // Aggiorna la UI
+}
+
+/**
+ * Rimuove un conto dalla lista dei conti.
+ * @param {number} id - L'ID del conto da rimuovere.
+ */
+export function remConto(id){
+    Conto.accountList.forEach((account, i) => { // Cerca il conto da rimuovere
+        if(account.id === id) Conto.accountList.splice(i, 1); // Rimuove il conto dalla lista
+    });
+    currentAccount = (Conto.accountList.length > 0)? Conto.accountList[0]: nullAccount; // Imposta il conto corrente al primo conto della lista o al conto di default
+    UI.updateAccountList(Conto.accountList, currentAccount); // Aggiorna la UI
+}
+
+/**
+ * Restituisce il conto corrente.
+ * @returns {Conto} Il conto corrente.
+ */
 export function getCurrAccount(){ return currentAccount; }
+
+/**
+ * Imposta il conto corrente.
+ * @param {Conto} account - Il nuovo conto corrente.
+ */
 export function setCurrAccount(account){
-    currentAccount = account;
-    UI.updateAccountList(Conto.accountList, currentAccount);
+    currentAccount = account; // Imposta il conto corrente
+    UI.updateAccountList(Conto.accountList, currentAccount); // Aggiorna la UI
 }
+
+/**
+ * Salva i dati nella memoria persistente.
+ */
 export function saveData(){
-    Memory.save()
+    Memory.save() // Salva i dati
 }
+
+/**
+ * Carica i dati dalla memoria persistente.
+ */
 export function loadData(){
-    Memory.load()
-    currentAccount = currentAccount ?? Conto.accountList[0] ?? nullAccount;
-    UI.updateAccountList(Conto.accountList, currentAccount);
-    UI.showConto(currentAccount);
+    Memory.load() // Carica i dati
+    currentAccount = currentAccount ?? Conto.accountList[0] ?? nullAccount; // Imposta il conto corrente al primo conto della lista o al conto di default
+    UI.updateAccountList(Conto.accountList, currentAccount); // Aggiorna la UI
+    UI.showConto(currentAccount); // Mostra il conto corrente
 }
+
+/**
+ * Elimina tutti i dati dalla memoria persistente.
+ */
 export function delData() {
-    Memory.warn_deleteAll();
+    Memory.warn_deleteAll(); // Elimina tutti i dati
+    loadData() // Ricarica i dati (che saranno vuoti)
+}
+
+/**
+ * Inizializza la memoria caricando i dati dai file JSON.
+ */
+function initMemory() {
+    /**
+     * Carica i dati da un file JSON e li aggiunge a una lista.
+     * @param {string} path - Il percorso del file JSON.
+     * @param {Array} list - La lista a cui aggiungere i dati.
+     * @param {class} Class - La classe degli oggetti da creare.
+     */
+    function loadFromJSON(path, list, Class) {
+        fetch(path) // Usa il percorso relativo al file JSON
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok'); // Se la risposta non è ok, lancia un errore
+                }
+                return response.json(); // Converte la risposta in JSON
+            })
+            .then(data => {
+                // Qui puoi usare i dati del JSON
+                console.log("data", data)
+                data.forEach(obj => { // Per ogni oggetto nel JSON
+                    let newObj = new Class(obj.name, obj.description, obj.parentCategoryID); // Crea un nuovo oggetto della classe specificata
+                    list.push(newObj) // Aggiunge l'oggetto alla lista
+                });
+                Memory.save() // Salva i dati nella memoria persistente
+            })
+            .catch(error => {
+                console.error('Errore durante il caricamento del file JSON:', error);
+            });
+    }
+    loadFromJSON("defaultData/categories.json", Category.categoryList, Category);
+    loadFromJSON("defaultData/payOptions.json", PaymentOptions.optionList, PaymentOptions);
     loadData()
 }
--- models/Conto.js
+++ models/Conto.js
@@ -1,75 +1,110 @@
-import * as UI from "../view/ui.js";
-import * as Memory from "./jsonManager.js"
+import * as Memory from "./jsonManager.js";
 
+/**
+ * Classe per rappresentare un conto.
+ */
 export class Conto {
-    static id = 0;
-    static accountList = [];
+    static accountList = []; // Lista statica di tutti i conti
+    static accountCounter = 0; // Contatore statico per generare ID univoci
     
-    constructor(name, value=0, currency="€") {
-        this._transactionList = [];
-        this._name = name;
-        this._value = parseInt(value);
-        this._currency = currency;
-        if(this._name !== "__none__") {
-            this._id = Conto.id;
-            Conto.id++;
-            Conto.accountList.push(this);
-            Memory.save()
-        } else {
-            this._id = -101;
-        }
+    /**
+     * Costruttore per la classe Conto.
+     * @param {string} name - Il nome del conto.
+     * @param {number} [startCash=0] - Il saldo iniziale del conto (opzionale, default: 0).
+     * @param {string} [currency='€'] - La valuta del conto (opzionale, default: '€').
+     */
+    constructor(name, startCash = 0, currency = '€') {
+        this._name = name; // Nome del conto
+        this._startCash = startCash; // Saldo iniziale
+        this._currency = currency; // Valuta
+        this._transactions = []; // Lista delle transazioni del conto
+        this._id = Conto.accountCounter; // ID univoco del conto
+        Conto.accountCounter++; // Incrementa il contatore per il prossimo conto
+        Conto.accountList.push(this); // Aggiunge il conto alla lista statica
+        Memory.save(); // Salva i dati nella memoria persistente
     }
-
+    
+    /**
+     * Aggiunge una transazione al conto.
+     * @param {Transaction} transaction - La transazione da aggiungere.
+     */
     addTransaction(transaction) {
-        this._transactionList.push(transaction);
-        this.value += parseInt(transaction.value);
+        this._transactions.push(transaction); // Aggiunge la transazione alla lista
+        Memory.save(); // Salva i dati nella memoria persistente
     }
-    removeTransaction(id) {
-        let foundedIndex;
-        this.transactionList.forEach((elem, i) => {
-            console.log("'"+elem.id+"'");
-            console.log("'"+id+"'");
-            if(parseInt(elem.id) === parseInt(id)){
-                foundedIndex = i;
-                console.log("1: "+this.value);
-                this.value -= elem.value;
-                console.log("2: "+this.value);
-            }
+    
+    /**
+     * Rimuove una transazione dal conto.
+     * @param {Transaction} transaction - La transazione da rimuovere.
+     */
+    remTransaction(transaction){
+        this._transactions.forEach((trans, i) => {
+            if(trans.id === transaction.id) this._transactions.splice(i, 1);
         });
-        this.transactionList.splice(foundedIndex, 1);
-        console.log(this.transactionList);
-        UI.showConto(this, "conto");
+        Memory.save(); // Salva i dati nella memoria persistente
     }
-
+    
+    /**
+     * Restituisce il nome del conto.
+     * @returns {string} Il nome del conto.
+     */
     get name() {
         return this._name;
     }
-    get value() {
-        return this._value;
-    }
-    get currency() {
-        return this._currency;
-    }
-    get transactionList() {
-        return this._transactionList;
-    }
-    get id() {
-        return this._id;
-    }
-
+    
+    /**
+     * Imposta il nome del conto.
+     * @param {string} value - Il nuovo nome del conto.
+     */
     set name(value) {
         this._name = value;
     }
-    set value(value) {
-        this._value = value;
+    
+    /**
+     * Restituisce il saldo del conto.
+     * @returns {number} Il saldo del conto.
+     */
+    get startCash() {
+        return this._startCash;
     }
+    
+    /**
+     * Imposta il saldo del conto.
+     * @param {number} value - Il nuovo saldo del conto.
+     */
+    set startCash(value) {
+        this._startCash = value;
+    }
+    
+    /**
+     * Restituisce la valuta del conto.
+     * @returns {string} La valuta del conto.
+     */
+    get currency() {
+        return this._currency;
+    }
+    
+    /**
+     * Imposta la valuta del conto.
+     * @param {string} value - La nuova valuta del conto.
+     */
     set currency(value) {
         this._currency = value;
     }
-    set transactionList(value) {
-        this._transactionList = value;
+    
+    /**
+     * Restituisce la lista delle transazioni del conto.
+     * @returns {Array<Transaction>} La lista delle transazioni.
+     */
+    get transactions() {
+        return this._transactions;
     }
-    set id(value) {
-        this._id = value;
+    
+    /**
+     * Restituisce l'ID del conto.
+     * @returns {number} L'ID del conto.
+     */
+    get id() {
+        return this._id;
     }
 }
