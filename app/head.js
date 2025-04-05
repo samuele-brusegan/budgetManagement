451d8449e11f758bca079c2908733b1c1c37d9d7
samuele-brusegan <samuele.brusegan@itiszuccante.edu.it>
2025-04-05 20:59:30 +0200

    Modified manifest.json

--- app/head.js
+++ app/head.js
@@ -1,11 +1,3 @@
 const head = document.head
 console.log(head)
-head.innerHTML = '<meta charset="UTF-8">\n' +
-    '        <meta name="viewport" content="width=device-width, initial-scale=1.0">\n' +
-    '        <title>Budget Management</title>\n' +
-    '        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">\n' +
-    '        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>\n' +
-    '        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>\n' +
-    '        <link rel="manifest" href="../manifest.json" />\n' +
-    '        <link rel="stylesheet" href="styles.css">\n' +
-    '        <link rel="icon" href="../favicon.ico">';
+head.innerHTML = '
--- models/Object.js
+++ models/Object.js
@@ -1,7 +1,10 @@
 class Object {
     static inventory = [];
-    constructor(name, quantity = 0, ){
-        
+    constructor(name, quantity = 0, tag, descr){
+        this._name = name;
+        this._quantity = quantity; 
+        this._tag = tag;
+        this._description = descr;
     }
 }
 
