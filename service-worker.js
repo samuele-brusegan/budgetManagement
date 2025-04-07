c17643a7e47c626dfa13f40c488873c59843d1be
Sapoleone <154547272+Sapoleone@users.noreply.github.com>
2025-04-07 15:55:26 +0200

    Finally fixed local cache

--- service-worker.js
+++ service-worker.js
@@ -1,69 +1,107 @@
 const CACHE_NAME = "spese-app-v1";
+
 const ASSETS_TO_CACHE = [
-    "index.html", 
-    "styles.css", 
-    "app.js", 
-    "view/home.js", 
-    "view/navbar.js", 
-    "controllers/Controller.js", 
-    "models/Account.js", 
-    "models/Transaction.js", 
-    "models/defaultData/defaultData.json", 
-    "assets/icons/icon-192x192.png", 
-    "assets/icons/icon-512x512.png", 
-    "manifest.json", 
-    "service-worker.js", 
-    "favicon.ico", 
-    
-    "app/developer-info.html", 
-    "app/feedback.html", 
-    "app/modify-element.html", 
-    "app/newElement.html", 
-    "app/notification.html", 
-    "app/settings.html", 
-    "app/transactions.html", 
-    "app/script.js", 
-    "app/specificScript/devInfo.js", 
-    "app/specificScript/home.js", 
-    "app/specificScript/notifications.js", 
-    "app/specificScript/transactions.js", 
-    "app/debug/index.html", 
-    "app/debug/inputCode.js", 
-    "app/debug/testPresets.json" 
+    // "README.md",
+    "app.js",
+    "cache-manifest.json",
+    "favicon.ico",
+    "index.html",
+    "manifest.json",
+    "placeholder.ico",
+    "service-worker.js",
+    "service-worker_old.js",
+    "styles.css",
+    "./app/developer-info.html",
+    "./app/feedback.html",
+    "./app/index.html",
+    "./app/inventory.html",
+    "./app/modify-element.html",
+    "./app/newElement.html",
+    "./app/notification.html",
+    "./app/script.js",
+    "./app/settings.html",
+    "./app/styles.css",
+    "./app/transaction.html",
+    "./app/debug/index.html",
+    "./app/debug/inputCode.js",
+    "./app/debug/testPresets.json",
+    "./app/specificScript/devInfo.js",
+    "./app/specificScript/home.js",
+    "./app/specificScript/notifications.js",
+    "./app/specificScript/transactions.js",
+    "./assets/byteLab.svg",
+    "./assets/placeholder.svg",
+    "./assets/favicons/android-chrome-192x192.png",
+    "./assets/favicons/android-chrome-512x512.png",
+    "./assets/favicons/apple-touch-icon.png",
+    "./assets/favicons/favicon-16x16.png",
+    "./assets/favicons/favicon-32x32.png",
+    "./assets/favicons/favicon.ico",
+    "./assets/icons/boxes.svg",
+    "./assets/icons/boxes_old.svg",
+    "./assets/icons/home.svg",
+    "./assets/icons/notification.svg",
+    "./assets/icons/searchicon.svg",
+    "./controllers/Controller.js",
+    "./controllers/debugCtrl.js",
+    "./models/Category.js",
+    "./models/Conto.js",
+    "./models/Object.js",
+    "./models/PaymentOptions.js",
+    "./models/Transaction.js",
+    "./models/defaultData",
+    "./models/memory.js",
+    "./models/defaultData/categories.json",
+    "./models/defaultData/payOptions.json",
+    "./view/CategoryUI.js",
+    "./view/ContoUI.js",
+    "./view/accountSelector.js",
+    "./view/navbar.js",
+    "./view/topbar.js",
+    "./view/ui.js"
 ];
 
 // Installazione del Service Worker
-self.addEventListener("install", (event) => {
-    console.log("[Service Worker] Install")
-    try {
-        event.waitUntil(
-            caches.open(CACHE_NAME).then((cache) => {
-                return cache.addAll(ASSETS_TO_CACHE);
-            })
-        );
-    } catch (e) {
-        console.error(e)
-    }
+self.addEventListener("install", (e) => {
+    console.log("[Service Worker] Install");
+    e.waitUntil(
+        (async () => {
+            const cache = await caches.open(CACHE_NAME);
+            console.log("[Service Worker] Caching all: app shell and content");
+            await cache.addAll(ASSETS_TO_CACHE);
+        })(),
+    );
 });
+
 
 // Attivazione del Service Worker
-self.addEventListener("activate", (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames
-                    .filter((name) => name !== CACHE_NAME)
-                    .map((name) => caches.delete(name))
-            );
-        })
+// self.addEventListener("activate", (event) => {
+//     event.waitUntil(
+//         caches.keys().then((cacheNames) => {
+//             return Promise.all(
+//                 cacheNames
+//                     .filter((name) => name !== CACHE_NAME)
+//                     .map((name) => caches.delete(name))
+//             );
+//         })
+//     );
+// });
+
+// Intercettazione delle richieste di rete
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        (async () => {
+            const r = await caches.match(e.request);
+            console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+            if (r) {
+                return r;
+            }
+            const response = await fetch(e.request);
+            const cache = await caches.open(CACHE_NAME);
+            console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+            await cache.put(e.request, response.clone());
+            return response;
+        })(),
     );
 });
 
-// Intercettazione delle richieste di rete
-self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
-});
