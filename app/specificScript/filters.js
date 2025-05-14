5ede2a394f7a280c70194777b4309fbd4930be92
samuele-brusegan <samuele.brusegan@itiszuccante.edu.it>
2025-05-14 08:29:13 +0200

    Added maskable icon, fixed privacy policy ui

---
+++ app/specificScript/filters.js
@@ -0,0 +1,8 @@
+import Cookie from "../../models/Cookie.js"
+//Get filters
+let filtersJSON = Cookie.get("filters")
+//come li salvo i filtri?
+
+//Print filters
+    //set listener close
+
---
+++ models/Filter.js
@@ -0,0 +1,11 @@
+export class Filter {
+    constructor(name, data_from, data_to, import_from, import_to, categories){
+        this.f_name        = name;
+        this.f_data_from   = data_from;
+        this.f_data_to     = data_to;
+        this.f_import_from = import_from;
+        this.f_import_to   = import_to;
+        this.f_categories  = categories;
+    }
+}
+
